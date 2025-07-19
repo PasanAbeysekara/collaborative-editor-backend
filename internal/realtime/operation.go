c8ae41b744e2e712345d5fe88c3d8d3f479327d1
PasanAbeysekara <pasankavindaabey@gmail.com>
2025-07-19 23:45:41 +0530

    feat: define ServerMessage and ServerMessageType for client communication

--- internal/realtime/operation.go
+++ internal/realtime/operation.go
@@ -2,6 +2,20 @@ package realtime
 
 type OpType string
 
+type ServerMessageType string
+
+const (
+	MsgInitialState ServerMessageType = "initial_state"
+	MsgOperation    ServerMessageType = "operation"
+)
+
+// wrapper for all messages sent to clients.
+type ServerMessage struct {
+	Type    ServerMessageType `json:"type"`
+	Content string            `json:"content,omitempty"` // For initial state
+	Op      *Operation        `json:"op,omitempty"`      // For operations
+}
+
 const (
 	OpInsert OpType = "insert"
 	OpDelete OpType = "delete"
